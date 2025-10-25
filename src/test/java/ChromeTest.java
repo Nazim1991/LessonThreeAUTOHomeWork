8f3b45643b53a1655bd8eac74f2ed20834063db9
BRONKS <nazim.said@yandex.ru>
2025-10-26 01:46:24 +0300

    Lucking resolved First part

--- src/test/java/ChromeTest.java
+++ src/test/java/ChromeTest.java
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ChromeTest {
 
@@ -41,13 +43,15 @@ class ChromeTest {
 
     @Test
     void shouldSubmitRequest() {
-        driver.get("http://localhost:9999");
-        WebElement form = driver.findElement(By.cssSelector("[data-test-id='callback-form']"));
-        form.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Василий");
-        form.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79270000000");
-        form.findElement(By.cssSelector("[data-test-id='agreement']")).click();
-        form.findElement(By.cssSelector("[data-test-id='submit']")).click();
-        String text = driver.findElement(By.className("alert-success")).getText();
-        assertEquals("Ваша заявка успешно отправлена!", text.trim());
+        List<WebElement> inputs = driver.findElements(By.cssSelector("input"));
+        inputs.get(0).sendKeys("Василий");
+        inputs.get(1).sendKeys("+79022090443");
+        driver.findElement(By.cssSelector("label")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        WebElement result = driver.findElement(By.className("alert-success"));
+        assertTrue(result.isDisplayed());
+        assertEquals("Ваша заявка успешно отправлена!", result.getText().trim());
     }
+
+
 }
